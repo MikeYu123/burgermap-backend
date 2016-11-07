4fad9364e51586d0e030ea1ca0e6d130342c6076
miks <mikesehse@gmail.com>
2016-11-07 16:08:56 +0300

    initialized all models

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107082125) do
+ActiveRecord::Schema.define(version: 20161107125249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "place_reviews", force: :cascade do |t|
+    t.string   "title"
+    t.string   "short_description"
+    t.string   "image"
+    t.float    "lat"
+    t.float    "lng"
+    t.text     "description"
+    t.float    "mark"
+    t.string   "link"
+    t.integer  "user_id"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
+
+  add_index "place_reviews", ["lat", "lng"], name: "index_place_reviews_on_lat_and_lng", using: :btree
+  add_index "place_reviews", ["mark"], name: "index_place_reviews_on_mark", using: :btree
+  add_index "place_reviews", ["title"], name: "index_place_reviews_on_title", using: :btree
+  add_index "place_reviews", ["user_id"], name: "index_place_reviews_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "provider",               default: "email", null: false
